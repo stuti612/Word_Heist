8e3da24ebc24f3f18bb3c76baf924a614fa7c8c6
sonalgehlot10 <sgehlot@usc.edu>
2026-02-24 11:53:10 -0800

    Update player movement to add space jump and added platforms

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
 
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private float moveInput;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
 
     void Start()
     {
@@ -14,13 +20,41 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement.Normalize();
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            // rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        // Debug.Log(isGrounded);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void LateUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+    }
+
+    // private void OnDrawGizmosSelected()
+    // {
+    //     if (groundCheck == null) return;
+    //     Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    // }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Spike"))
+        {
+            transform.position = new Vector3(-7f, -3f, 0f);
+            rb.linearVelocity = Vector2.zero;
+        }
     }
 }
